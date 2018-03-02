308c953d157ca19993bbd4c9635a964adb1689d4
Krzysztof Sliwinski <kpsliwinski@gmail.com>
2018-03-02 22:38:45 +0100

    Create project - part 2

--- src/index.js
+++ src/index.js
@@ -1,14 +1,19 @@
 import React from 'react';
 import { render } from 'react-dom';
+import { Router, Route, hashHistory } from 'react-router';
 import { Provider } from 'react-redux';
 import store from './store';
+import routes from './routes';
+
 import DevTools from './DevTools';
 
+import './country.css';
 
 render(
     <Provider store={store}>
       <div>
-        <h1>Inicjalizacja projektu</h1>
+        <Router history={hashHistory} routes={routes}/>
+
         <DevTools />
       </div>
     </Provider>,
